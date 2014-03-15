33428187e4dd7ec5c8a995a4fffd5091de9bca88
RushyMCFC <rushymcfc@gmail.com>
2014-03-15 19:47:06 +0000

    deleted
    unwanted files

--- README.md
+++
@@ -1,4 +0,0 @@
-Facebook-Hackathon-15-03-14
-===========================
-
-Git for Facebook Hackathon 15-16th March '14
--- Todo Extension/firststage.js
+++
@@ -1,6 +0,0 @@
-alert("hi");
-var s = document.createElement('script');
-
-s.innerHtml = "alert('gogo');";
-
-document.body.appendChild(s);
