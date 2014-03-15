5859dea1e61fd45e2ec413e6316f597eca2bbb94
mhollands <mh739@cam.ac.uk>
2014-03-15 22:46:45 +0000

    textbox

--- Todo Extension/runscript.js
+++ Todo Extension/runscript.js
@@ -1,5 +1,28 @@
-﻿function func() {
-    
+﻿
+/*
+var connection = new ActiveXObject("ADODB.Connection");
+
+var connectionstring = "Data Source=sql2.freesqldatabase.com;Initial Catalog=sql233229;User ID=sql233229;Password=VN2URbE4q9Qm;";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+var command = 'CREATE TABLE table_name(col1 int);';
+
+rs.Open("CREATE", connection);
+rs.MoveFirst
+while (!rs.eof) {
+    document.write(rs.fields(1));
+    rs.movenext;
+}
+
+rs.close;
+connection.close;
+
+//myFunction();
+*/
+function func() {
+
     var isEvent = (document.URL.indexOf("https://www.facebook.com/events/") == 0);
 
     var rems = null;
@@ -35,8 +58,6 @@
         //create the overall div
         var div = document.createElement('div');
         div.setAttribute("id", "todoList");
-
-
 
         //go through every event
         for (var i = 0; i < events.length; i++) {
@@ -79,6 +100,22 @@
         }
 
         if (isEvent) {
+            
+            var createbox = document.createElement('input');
+            createbox.setAttribute('type', 'text');
+            createbox.setAttribute('value', 'Create new task...');
+            createbox.style.marginLeft = "5em";
+            createbox.onfocus = createbox_focussed;
+            div.insertBefore(createbox);
+            
+            //var frame = document.createElement('iframe');
+            //frame.src = "http://stackoverflow.com/questions/18228993/textbox-default-text-make-dissapear-on-focus";
+            //div.insertBefore(frame);
+        }
+       
+
+        //add it to the page depending on whether its newsfeed or event
+        if (isEvent) {
 			rems.insertBefore(div, document.getElementById("event_description"));
 			//rems.insertBefore(div);
             div.style.backgroundColor = "white";
@@ -91,9 +128,8 @@
     else {
        //alert("not found");
     }
-}
 
-//func();
+}
 
 window.setInterval(function () {
     if(document.getElementById("todoList")==null)
@@ -102,10 +138,16 @@ window.setInterval(function () {
     }
 }, 1000);
 
+var myfileurl = "http://www.this-page-intentionally-left-blank.org/";
+
+function createbox_focussed(e)
+{
+    alert(e.innerText);
+}
+
 function checkbox_toggle()
 {
     alert("toggle");
-
 }
 
 function getEventIDFromUrl()
@@ -115,3 +157,24 @@ function getEventIDFromUrl()
 }
 
 //document.getElementsByClassName("accordion")[0].accordion();
+
+function myFunction()
+{
+    loadjscssfile("parse-1.2.13.min.js", "js") //dynamically load and add this .js file
+
+    Parse.$ = jQuery;
+    Parse.initialize("3bWxW7KCxkHuf0OeM2s3KhTneI1j23ZnVu3hfvzK","RZ0WWQQFWQa9tFPyPreB1V9Amu0ZGnWzE0X8y48t");
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.set("eventId","00001");
+    testObject.set("eventName","test event");
+    testObject.save();
+    //testObject.save({foo: "bar111"}).then(function(object) {
+    alert("yay! it worked");
+    alert(testObject.get("eventId"));
+    alert(testObject.get("eventName"));
+
+    //});
+}
+
+
